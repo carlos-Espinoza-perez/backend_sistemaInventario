8bc032c77f2ac1731141dfdd9536c37b376c00ab
Carlos Espinoza <cpespinoza044@gmail.com>
2025-07-02 21:29:54 -0600

    fix: add type .env

--- db/database.py
+++ db/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./inventario.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
